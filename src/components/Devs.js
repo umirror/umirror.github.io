bf5f5219787818f63abf71f3f267b077e72ec1d3
Enzo Enrico <e.enrico2005@gmail.com>
2022-10-25 09:08:31 -0300

    adicionado estilos

--- src/components/Devs.js
+++ src/components/Devs.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Css/Devs.css";
 import img from "./resources/👍.jpg"
+import enzo from "./resources/enzo.jpeg"
+import julio from "./resources/julio.jpeg"
+import luiz from "./resources/luiz.jpg"
+import lucas from "./resources/luas.jpeg"
+import joao from "./resources/joao.jpeg"
 import { FaTwitter } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
 import { FaInstagram } from "react-icons/fa"
@@ -10,68 +15,105 @@ function Devs() {
     return (
         <div className="main-body">
             <h1>Conheça nosso time!</h1>
-            <div className="main-wrapper">
-                <div class="card">
-                    <div class="image">
-                        <img src={img} />
-                    </div>
-                    <div class="info_card">
-                        <h4>Enzo Enrico</h4>
-                        <h5>Lead Developer</h5>
-                    </div>
-                    <div class="socials">
-                        <a href="https://twitter.com/ky0uko__">
-                            <FaTwitter />
-                        </a>
-                        <a href="https://github.com/enzoenrico">
-                            <FaGithub />
-                        </a>
-                        <a href="https://www.instagram.com/ky0uko_/">
-                            <FaInstagram />
-                        </a>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="image">
-                        <img src={img} />
-                    </div>
-                    <div class="info_card">
-                        <h4>Enzo Enrico</h4>
-                        <h5>Lead Developer</h5>
-                    </div>
-                    <div class="socials">
-                        <a href="https://twitter.com/ky0uko__">
-                            <FaTwitter />
-                        </a>
-                        <a href="https://github.com/enzoenrico">
-                            <FaGithub />
-                        </a>
-                        <a href="https://www.instagram.com/ky0uko_/">
-                            <FaInstagram />
-                        </a>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="image">
-                        <img src={img} />
-                    </div>
-                    <div class="info_card">
-                        <h4>Enzo Enrico</h4>
-                        <h5>Lead Developer</h5>
-                    </div>
-                    <div class="socials">
-                        <a href="https://twitter.com/ky0uko__">
-                            <FaTwitter />
-                        </a>
-                        <a href="https://github.com/enzoenrico">
-                            <FaGithub />
-                        </a>
-                        <a href="https://www.instagram.com/ky0uko_/">
-                            <FaInstagram />
-                        </a>
-                    </div>
-                </div>
 
+            <div className="main-wrapper">
+                <div className="card">
+                    <div className="image">
+                        <img src={enzo} />
+                    </div>
+                    <div className="info_card">
+                        <h4>Enzo Enrico</h4>
+                        <h5>Lead Developer</h5>
+                    </div>
+                    <div className="socials">
+                        <a href="https://twitter.com/ky0uko__">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://github.com/enzoenrico">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.instagram.com/ky0uko_/">
+                            <FaInstagram />
+                        </a>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src={luiz} />
+                    </div>
+                    <div className="info_card">
+                        <h4>Luiz Guilherme</h4>
+                        <h5>Front-End Developer</h5>
+                    </div>
+                    <div className="socials">   
+                        <a href="https://twitter.com/oLyghtz">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://github.com/yLight">
+                            <FaGithub />
+                        </a>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src={julio} />
+                    </div>
+                    <div className="info_card">
+                        <h4>Júlio César</h4>
+                        <h5>Analytic Developer</h5>
+                    </div>
+                    <div className="socials">
+                        <a href="https://twitter.com/ky0uko__">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://github.com/enzoenrico">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.instagram.com/ky0uko_/">
+                            <FaInstagram />
+                        </a>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src={lucas} />
+                    </div>
+                    <div className="info_card">
+                        <h4>Lucas Gabriel</h4>
+                        <h5>Device Architeture</h5>
+                    </div>
+                    <div className="socials">
+                        <a href="https://twitter.com/ky0uko__">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://github.com/enzoenrico">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.instagram.com/ky0uko_/">
+                            <FaInstagram />
+                        </a>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src={joao} />
+                    </div>
+                    <div className="info_card">
+                        <h4>João Manuel</h4>
+                        <h5>Product Designer</h5>
+                    </div>
+                    <div className="socials">
+                        <a href="https://twitter.com/ky0uko__">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://github.com/enzoenrico">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.instagram.com/ky0uko_/">
+                            <FaInstagram />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./Css/Home.css"
 
 function Home() {
-    console.log("")
-    return(
+
+    return (
         <div className="main">
+            {/* <div className="intro">
+                <h1 className="logo_header">
+                    <span className="logo">u</span><span className="logo">Mirror</span>
+                </h1>
+            </div> */}
+
             <section className="welcome">
                 <div className="welcome-wrapper">
-                    <h3>uMirror</h3>
+                    <h3>
+                        <p className="welcome-logo">u</p>
+                        <p className="welcome-logo">Mirror</p>
+                    </h3>
                     <h4>O espelho que cuida de você</h4>
                 </div>
-            <div className="scroll"></div>
+                <div className="scroll"></div>
             </section>
         </div>
     )
